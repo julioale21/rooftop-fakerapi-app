e7e571ce7891d5e249451ecee2a9f1a8e1583027
Julio Romero <julio_ale21@hotmail.com>
2021-07-19 17:07:17 -0300

    chore: added custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
-  initialColorMode: "light",
+  initialColorMode: "dark",
   useSystemColorMode: false,
 };
 const theme = extendTheme({ config });
