714c0a197c01233c7cbd7b4746e90c0200f2bdc3
Julio Romero <julio_ale21@hotmail.com>
2021-07-19 17:45:14 -0300

    chore: added user list item component

---
+++ src/components/UserListItem/UserListItem.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Stack, Image, Text, Grid, Center, Spacer } from "@chakra-ui/react";
+import User from "../../models/User";
+import TextInfo from "../TextInfo";
+
+interface Props {
+  user: User;
+}
+
+const UserListItem: React.FC<Props> = ({ user }) => {
+  return (
+    <Stack border="1px solid gray" paddingY={5}>
+      <Stack direction="row" justifyContent="center">
+        <Image borderRadius="full" height={150} src={user.image} width={150} />
+      </Stack>
+      <Stack direction="row" justifyContent="center">
+        <Text fontSize="1.5rem" fontWeight="bold" marginTop={3} textColor="gray.500">
+          {user.username}
+        </Text>
+      </Stack>
+      <Spacer />
+      <Grid gap={2} templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }}>
+        <Center>
+          <TextInfo title="First Name" value={user.firstname} />
+        </Center>
+        <Center>
+          <TextInfo title="Last Name" value={user.lastname} />
+        </Center>
+        <Center>
+          <TextInfo title="Email" value={user.email} />
+        </Center>
+        <Center>
+          <TextInfo title="Web Site" value={user.website} />
+        </Center>
+      </Grid>
+    </Stack>
+  );
+};
+
+export default UserListItem;
