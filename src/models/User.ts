6b3d7ff0a4d6aca3c44c1c1d14e02c0fbf829303
Julio Romero <julio_ale21@hotmail.com>
2021-07-19 17:08:07 -0300

    chore: added User interface

---
+++ src/models/User.ts
@@ -0,0 +1,12 @@
+export default interface User {
+  uuid: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+  password: string;
+  email: string;
+  ip: string;
+  mackAddress: string;
+  website: string;
+  image: string;
+}
