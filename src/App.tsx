ddfd063b136802683922791176732fb445e4397e
Julio Romero <julio_ale21@hotmail.com>
2021-07-21 17:54:03 -0300

    chore: added layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
 import ProductsView from "./pages/ProductsView";
 import UsersView from "./pages/UsersView";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/users">
-          <UsersView />
-        </Route>
-        <Route path="/products">
-          <ProductsView />
-        </Route>
-        <Route path="/">
-          <Redirect to="/users" />
-        </Route>
-      </Switch>
+      <AppLayout>
+        <Switch>
+          <Route path="/users">
+            <UsersView />
+          </Route>
+          <Route path="/products">
+            <ProductsView />
+          </Route>
+          <Route path="/">
+            <Redirect to="/users" />
+          </Route>
+        </Switch>
+      </AppLayout>
     </Router>
   );
 }
