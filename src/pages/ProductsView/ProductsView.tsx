f5a02f358c44c73c708bc2dd052c87e2e6b8c566
Julio Romero <julio_ale21@hotmail.com>
2021-07-21 17:56:08 -0300

    feat(pagination): added pagination to product list

--- src/pages/ProductsView/ProductsView.tsx
+++ src/pages/ProductsView/ProductsView.tsx
@@ -5,32 +5,57 @@ import { Text } from "@chakra-ui/layout";
 import axios from "axios";
 import Product from "../../models/Product";
 import ProducList from "../../components/products/ProducList";
+import Pagination from "../../components/Pagination";
 
-const productsURL = "https://fakerapi.it/api/v1/products";
+const productsURL = "https://fakerapi.it/api/v1/products?_quantity=100";
 
 const ProductsView: React.FC = () => {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [paginatedProducts, setPaginatedProducts] = useState<Product[]>([]);
+  const [productsPerPage] = useState(10);
+  const [offset, setOffset] = useState(0);
+  const [pageCount, setPageCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     async function fetchProducts() {
       try {
         const response = await axios.get(productsURL);
+        const productsList: Product[] = response.data.data;
+        const paginatedProducts: Product[] = productsList.slice(offset, offset + productsPerPage);
 
-        setProducts(response.data.data);
+        setPaginatedProducts(paginatedProducts);
+        setPageCount(Math.ceil(productsList.length / productsPerPage));
+        if (offset > 0) {
+          setCurrentPage(offset / productsPerPage);
+        }
       } catch (error) {
         console.log(error);
-        setProducts([]);
+        setPaginatedProducts([]);
       }
     }
     fetchProducts();
-  }, []);
+  }, [offset, productsPerPage]);
+
+  const handleNextPage = () => {
+    setOffset(offset + productsPerPage);
+  };
+
+  const handlePrevPage = () => {
+    setOffset(offset - productsPerPage);
+  };
 
   return (
     <Stack>
       <Text fontSize="2rem" marginY={6} textAlign="center">
         Product List
       </Text>
-      <ProducList products={products} />
+      <ProducList products={paginatedProducts} />
+      <Pagination
+        handleNext={handleNextPage}
+        handlePrev={handlePrevPage}
+        page={currentPage}
+        pageCount={pageCount}
+      />
     </Stack>
   );
 };
