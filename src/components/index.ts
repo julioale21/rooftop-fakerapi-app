a869a7aeb0215a0907167223dcde91016dddb0f9
Julio Romero <julio_ale21@hotmail.com>
2021-07-21 21:53:19 -0300

    chore: exported components

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as AppLayout } from "./AppLayout";
+export { default as Header } from "./Header";
+export { default as Pagination } from "./Pagination";
+export { default as ProductList } from "./products/ProducList";
+export { default as ProductListItem } from "./products/ProductListItem";
+export { default as TextInfo } from "./shared/TextInfo";
+export { default as UserList } from "./users/UserList";
+export { default as UserListItem } from "./users/UserListItem";
