a2c9435c20c216b71f1c0b746259c1f51c0574f4
Julio Romero <julio_ale21@hotmail.com>
2021-07-21 21:54:06 -0300

    added users folder

---
+++ src/components/users/UserList/userList.tsx
@@ -0,0 +1,20 @@
+import { Stack } from "@chakra-ui/react";
+import React from "react";
+import User from "../../../models/User";
+import { UserListItem } from "../../../components";
+
+interface Props {
+  users: User[];
+}
+
+const userList: React.FC<Props> = ({ users }) => {
+  return (
+    <Stack direction="column" margin={5}>
+      {users.map((user) => (
+        <UserListItem key={user.uuid} user={user} />
+      ))}
+    </Stack>
+  );
+};
+
+export default userList;
